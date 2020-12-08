85aa7ce18946d72533f44e7da94018cb6804873f
povnuj <povnuj@gmail.com>
2020-12-08 20:42:45 +0200

    Changed all logic to move AI

--- js/script.js
+++ js/script.js
@@ -1,33 +1,36 @@
 let MasSize = 3;
 let Deep = 2;
 MasSize--;
-let s = 0;
+let s = 0, df = 0;
 let Point = 0;
-let Player1 = [];
-let Player2 = [];
-let arr = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0]
-];
-function AddArrTomove(Player, a, b, Point) {
+let Player1 = [[]];
+let Player2 = [[]];
+let arr = [];
+    
+function AddArrTomove(Player, a, b, Point, operation) {
+    
     if (Player == 1) {
-        Player1.unshift(a + ',' + b + ',' + Point);
-    }
+        Player1[Player1.length] = [a, b, Point, operation];
+        
+        //df++;
+        //Player1.unshift(a + ',' + b + ',' + Point+','+operation);
+        
+        }
     else {
-        Player2.unshift(a + ',' + b + ',' + Point);
+        
+         Player2[Player2.length] = [a, b, Point, operation];
+        
+        //Player2.unshift(a + ',' + b + ',' + Point+','+operation);
     }
+   // if(Player1[0].length == 0) Player1.slice(0, 1);
+   // if(Player2[0].length == 0) Player2.slice(0, 1);
     Point = 0;
+
 }
-function PriorToMove(P, TmpArr, a, b) {
+let count = -1;
+function PriorToMove(P, TmpArr, a, b, o) {
     let P2;
 
-    /*    if (P == 1) {
-            P2 = 2;
-        }
-        else {
-            P2 = 1;
-        }*/
     switch (P) {
         case 1:
             P2 = 2;
@@ -36,135 +39,182 @@ function PriorToMove(P, TmpArr, a, b) {
             P2 = 1;
             break
         case 0:
-            //   P2 = 1;
+            P2 = 1;
             break
 
     }
+    //console.log('tmp',TmpArr);
     switch (TmpArr) {
         case P:
-            s--;
-            Point++;
+            count++;
+            //if(count > MasSize) alert("WIN PLAYER:"+P);
+           // console.log(count);
             break
         case P2:
-            Point = 0;
+            
+            count = -1;
             break
         case 0:
-            AddArrTomove(P, a, b, Point);
-            Point = 0;
+            count++;
+           // console.log(count);
+            AddArrTomove(P, a, b, count, o);
+            count = -1;
+            //alert("yep");
             break
     }
-    //   console.log(a, b,'s =',s, 'P =', Point);
-
 }
+
+
 
 function CheckVariant(a, b, Player, CheckPoint) {
 
-    ///Check ROW 
-
-    s = 0; Point = 0;
-    for (let i = b; i <= MasSize; i++) {
-        PriorToMove(Player, arr[a][i], a, i);
-        s++;
-
-        if (s == CheckPoint) {
-            i = MasSize;
-        }
-        //console.log('s = ',s, CheckPoint);
-    }
-
-    s = 0; Point = 0;
-    for (let i2 = b; i2 >= 0; i2--) {
-        PriorToMove(Player, arr[a][i2], a, i2);
-        s++;
-
-        if (s == CheckPoint) {
-            i2 = 0;
-        }
-    }
-
-    ///// check Column
-
-    s = 0; Point = 0;
-    for (let i = a; i <= MasSize; i++) {
-        PriorToMove(Player, arr[i][b], i, b);
-        s++;
-
-        if (s == CheckPoint) {
-            i = MasSize;
-        }
-    }
-
-    s = 0; Point = 0;
-    for (let i2 = a; i2 >= 0; i2--) {
-        PriorToMove(Player, arr[i2][b], i2, b);
-        s++;
-
-        if (s == CheckPoint) {
-            i2 = 0;
-        }
-    }
-
-    // -------------check x1
-
-    if (a == b) {
-        s = 0; Point = 0;
-        for (let i = a; i <= MasSize; i++) {
-            PriorToMove(Player, arr[i][i], i, i);
-            s++;
-
-            if (s == CheckPoint) {
-                i = MasSize;
-            }
-        }
-
-
-        s = 0; Point = 0;
-        for (let i = a; i >= 1; i--) {
-            PriorToMove(Player, arr[i][i], i, i);
-            s++;
-            //console.log(i);
-            if (s == CheckPoint) {
-                i = MasSize;
-            }
-        }
-    }
-    ////---------------------Check x2
-
-    let is = MasSize;
-    for (let i = 0; i <= MasSize; i++) {
-        if (a == i && b == is) {
-            let i3 = b; s = 0; Point = 0;
-            for (let i2 = a; i2 <= MasSize; i2++) {
-                PriorToMove(Player, arr[i2][i3], i2, i3);
-                s++;
-                i3--;
-                //console.log(i2);
-                if (s == CheckPoint) {
-                    i2 = MasSize;
-                }
-
-            }
-
-            i3 = b; s = 0; Point = 0;
-            for (let i2 = a; i2 >= 0; i2--) {
-                PriorToMove(Player, arr[i2][i3], i2, i3);
-                s++;
-                i3++;
-                //console.log(i2);
-                if (s == CheckPoint) {
-                    i2 = MasSize;
-                }
-
-            }
-
-
-        }
-        is--;
-    }
-
 }
 
 
+function checkVariants(player, a, b, variants, deeps) {
+    // -------------- Check col 
+    //---------------- UP
+   
+
+    if (variants == 0) {
+        
+        if(deeps == Deep) count = -1;
+        if (deeps <= 0 || a <= 0 || arr[a][b] == 0) {
+            console.log( 'u',a, b, player);
+            variants++;
+            deeps = Deep;
+           
+            PriorToMove(player, arr[a][b], a, b, 0);
+            a = chA; b = chB;
+        }
+        else {
+            console.log( 'u',a, b, player);
+            PriorToMove(player, arr[a][b], a, b, 0);
+            return checkVariants(player, a - 1, b, variants, deeps - 1);
+        }
+    }
+    /////----------------Down
+
+    if (variants == 1) {
+       
+        if(deeps == Deep) count = -1;
+    
+        if (deeps <= 0 || a >= MasSize || arr[a][b] == 0) {
+            console.log( 'd',a, b, player);
+            variants++;
+            deeps = Deep;
+            PriorToMove(player, arr[a][b], a, b, 0);
+            a = chA; b = chB;
+        }
+        else {
+            console.log( 'd',a, b, player);
+            PriorToMove(player, arr[a][b], a, b, 0);
+            return checkVariants(player, a + 1, b, variants, deeps - 1);
+        }
+    }
+    // -------------- Check row 
+    //---------------- Left
+    if (variants == 2) {
+        if(deeps == Deep) count = -1;
+        if (deeps <= 0 || b <= 0 || arr[a][b] == 0) {
+            variants++;
+            deeps = Deep;
+            PriorToMove(player, arr[a][b], a, b, 1);
+            a = chA; b = chB;
+        }
+        else {
+            PriorToMove(player, arr[a][b], a, b, 1);
+            return checkVariants(player, a, b - 1, variants, deeps - 1);
+        }
+    }
+    /////----------------Right
+
+    if (variants == 3) {
+        if(deeps == Deep) count = -1;
+        if (deeps <= 0 || b >= MasSize || arr[a][b] == 0) {
+            variants++;
+            deeps = Deep;
+            PriorToMove(player, arr[a][b], a, b, 1);
+            a = chA; b = chB;
+        }
+        else {
+            PriorToMove(player, arr[a][b], a, b, 1);
+            return checkVariants(player, a, b + 1, variants, deeps - 1);
+        }
+    }
+// -------------- Check \ 
+//---------------- Right
+
+    if (variants == 4) {
+    //    console.log(deeps)
+      //  console.log(a,b)
+        if(deeps == Deep) count = -1;
+        if (deeps <= 0 || a >= MasSize || arr[a][b] == 0) {
+            variants++;
+            deeps = Deep;
+             PriorToMove(player, arr[a][b], a, b, 2);
+            a = chA; b = chB;
+        }
+        else {
+             PriorToMove(player, arr[a][b], a, b, 2);
+            return checkVariants(player, a + 1, b + 1, variants, deeps - 1);
+        }
+    }
+    
+/////----------------Left
+
+    if (variants == 5) {
+        //console.log(a, b)
+        if(deeps == Deep) count = -1;
+        if (deeps <= 0 || a <= 0  || arr[a][b]  == 0) {
+            variants++;
+            deeps = Deep;
+             PriorToMove(player, arr[a][b], a, b, 2);
+            a = chA; b = chB;
+        }
+        else {
+             PriorToMove(player, arr[a][b], a, b, 2);
+            return checkVariants(player, a - 1, b - 1, variants, deeps - 1);
+        }
+        
+    }
+
+// -------------- Check / 
+//---------------- Right
+    if (variants == 6 && chA + chB == MasSize) {
+        if(deeps == Deep) count = -1;
+        if (deeps <= 0 || b <= 0 || a >= MasSize || arr[a][b] == 0) {
+            variants++;
+            deeps = Deep;
+            PriorToMove(player, arr[a][b], a, b, 3);
+            a = chA; b = chB;
+        }
+        else {
+            PriorToMove(player, arr[a][b], a, b, 3);
+            return checkVariants(player, a + 1, b - 1, variants, deeps - 1);
+        }
+    }
+    /////----------------Left
+
+    if (variants == 7 && chA + chB == MasSize) {
+        if(deeps == Deep) count = -1;
+       
+        if (deeps <= 0 || b >= MasSize || a <= 0 || arr[a][b] == 0) {
+            variants++;
+            deeps = Deep;
+            PriorToMove(player, arr[a][b], a, b, 3);
+            a = chA; b = chB;
+        }
+        else {
+            PriorToMove(player, arr[a][b], a, b, 3);
+            return checkVariants(player, a - 1, b + 1, variants, deeps - 1);
+        }
+    }
+
+    return 0;
+}
+
+let chA, chB;
 
 function checkArr(a, b, c) {
 
@@ -180,24 +230,29 @@ function checkArr(a, b, c) {
     else {
 
         if (arr[a][b] == '1') {
-            //console.log(a, b);
-            CheckVariant(a, b, 1, Deep);
+            chA = a; chB = b;
+            checkVariants(1, a, b, 0, Deep);
         }
         if (arr[a][b] == '2') {
-            //console.log(a, b);
-            CheckVariant(a, b, 2, Deep);
+            chA = a; chB = b;
+            checkVariants(2, a, b, 0, Deep);
         }
         if (arr[a][b] == '0' && c == 1) {
-            console.log('P11 = ', a,b);
-            EditGrid(a, b, 1, 0);
-            
+            //            console.log('P11 = ', a,b);
+            editGrid(a, b, 1, 0);
+            c = 0;
+
         }
+
+
         return checkArr(a, b - 1);
 
     }
 
 }
-
+//checkArr(2, 0, 0);
+//console.log(Player1)
+/// --------Create HTML grid and matrix arr
 function CreateGrid() {
     let XODiv = document.querySelector('.XO');
 
@@ -206,69 +261,108 @@ function CreateGrid() {
         let div = document.createElement('div');
         div.className = "Rows" + i + ' format';
         RowDiv.append(div);
+        arr[i] = [];
 
 
         for (let i2 = 0; i2 <= MasSize; i2++) {
             let RowDiv = document.querySelector('.Rows' + i);
             let div = document.createElement('div');
-            div.className = "Tab";
+
+            switch(i){
+                case 0:
+                    if(i2 == 0) div.className = "Tab borderTop borderL";
+                    else if(i2 == MasSize) div.className = "Tab borderTop borderR";
+                    else div.className = "Tab borderTop";
+                    break
+                case MasSize:
+                    if(i2 == MasSize) div.className = "Tab borderBot borderR";
+                    else if(i2 == 0) div.className = "Tab borderBot borderL";
+                    else div.className = "Tab borderBot";
+                    break
+                default:
+                    if(i2 == MasSize) div.className = "Tab borderR";
+                    else if(i2 == 0) div.className = "Tab  borderL";
+                    else div.className = "Tab ";
+            }
+           
+
             div.id = +i + ',' + i2;
-            div.innerHTML = "-";
             RowDiv.append(div);
+            arr[i][i2] = 0;
         }
     }
 }
 
 CreateGrid();
-function EditGrid(a, b, c, c1) {
+
+
+
+function editGrid(a, b, c, c1) {
     arr[a][b] = c;
     let XODiv = document.getElementById(a + ',' + b);
     XODiv.textContent = c1;
 }
+function verifyPlayersMove(player, p, o){
+    let tmpP = [[]], tmpP2 = [[]];
+    
+    if(player.length != 0){
+        for(let i = 1; i <= player.length -1  ; i++){
+            if(o == 1 || o == 2 ){
+                if(tmpP[0].length == 0) tmpP[0] = player[i];
+                if(tmpP[0][2] < player[i][2]) tmpP[0] = player[i];
+               // console.log('tmp',tmpP[0])
+            }
+            else{
+                for(let j = i + 1; j <= player.length - 1 ; j++){
+                    
+                    if(player[i].join('') == player[j].join('')) tmpP[0] = player[j];
+                }
+                
+            }
+        }
+    }
+    console.log('pl',p, tmpP[0][2]);
+    if(o == 1) return tmpP[0][2];
+    if(o == 2) {
+        editGrid(tmpP[0][0], tmpP[0][1], 1, 0);
+    return tmpP;
+    }
+    if(tmpP[0].length != 0) AddArrTomove(p, tmpP[0][0], tmpP[0][1], tmpP[0][2]++, tmpP[0][3]);
+    
+    //return ;
+    
+}
 
 function MoveAI() {
-    let TmpArr = [], TmpArr2 = [], TmpArr3 = [];
+    //console.log("o",Player1);
+    verifyPlayersMove(Player1, 1, 0);
+    verifyPlayersMove(Player2, 2, 0);
+console.log("==",verifyPlayersMove(Player2, 2, 1), verifyPlayersMove(Player1, 1, 1));
+if(verifyPlayersMove(Player1, 1, 1) == undefined){
+    if(arr[MasSize / 2][MasSize / 2] == 0) editGrid(MasSize / 2, MasSize / 2, 1, 0);
+    else verifyPlayersMove(Player2, 2, 2);
+}   
+    if(verifyPlayersMove(Player2, 2, 1) == verifyPlayersMove(Player1, 1, 1)){
+        verifyPlayersMove(Player1, 1, 2);
+   }
 
-    for (let i = 0; i <= Player1.length - 1; i++) {
-        TmpArr = Player1[i].split(',');
-
-        if (TmpArr2.length == 0) TmpArr2 = TmpArr;
-        if (TmpArr[2] > TmpArr2[2]) TmpArr2 = TmpArr;
+    if(verifyPlayersMove(Player2, 2, 1) > verifyPlayersMove(Player1, 1, 1)){
+         verifyPlayersMove(Player2, 2, 2);
     }
-    for (let i = 0; i <= Player2.length - 1; i++) {
-        TmpArr = Player2[i].split(',');
-
-        if (TmpArr3.length == 0) TmpArr3 = TmpArr;
-        if (TmpArr[2] > TmpArr3[2]) TmpArr3 = TmpArr;
-    }
- 
-    if (TmpArr2[2] < TmpArr3[2]) {
-        EditGrid(TmpArr3[0], TmpArr3[1], 1, 0);
-    }
- 
-    if(Player1.length == 0){
-        if (arr[MasSize / 2][MasSize / 2] == 0) {
-            EditGrid(MasSize / 2, MasSize / 2, 1, 0);
-            console.log(TmpArr2);
-        }
-        else {
-            checkArr(MasSize, MasSize, 1);
-        }
+  
 }
- //   Player1.length = 0;
- //   Player2.length = 0;
-    // console.log(arr);
-}
-
 function MovePlayer(id) {
     let a = 123, b = 123;
     id.split(',').map(simbol => { a == 123 ? a = simbol : b = simbol })
-    EditGrid(a, b, 2, 'X');
+    editGrid(a, b, 2, 'X');
     checkArr(MasSize, MasSize, 0);
-    MoveAI();
     console.log('p1-', Player1);
     console.log('p2-', Player2);
-  
+    console.log('arr-', arr);
+    MoveAI();
+    Player1 = [[]];
+    Player2 = [[]];
+
 }
 
 const ClickByid = document.querySelectorAll('.Tab')
